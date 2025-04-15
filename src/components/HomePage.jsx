d15294706978d2abd01cca0133319ff1f6063009
Rodel <rodelg.nercuit@gmail.com>
2025-04-15 22:06:09 +0800

    Plotting Form initial Update

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/map.css';
 import Navigation from './Navigation';
 import LeafletMap from './LeafletMap';
+import Plot from './Plot';
 
 const HomePage = ( props ) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <div className="home-container">
         <Navigation 
          logoutClick = {props.onLogout}
          />
+
+         <Plot />
+
 
         <div className="leaflet-wrapper">
         <LeafletMap />
