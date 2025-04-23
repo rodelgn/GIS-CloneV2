fd2e0d9a46d6abf83aa8211067f507d594ddeb73
Rodel <rodelg.nercuit@gmail.com>
2025-04-23 12:31:13 +0800

    Create Account Style and Form Update

--- src/components/AccountCreation.jsx
+++ src/components/AccountCreation.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles/createUser.css';
 
 const AccountCreation = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+
   return (
     <div>
         <h1>Create Account</h1>
 
-        <form action="" className='account-form'>
+        <form action="submit" className='account-form'>
             <label>Name</label>
-            <input type="text" name='name' required/>
+            <input type="text" name='name' value='name' onChange={handleChange} required/>
 
             <label>Email</label>
-            <input type="text" name='email' required/>
+            <input type="text" name='email' value='email' onChange={handleChange} required/>
 
             <label>Password</label>
-            <input type="password" name='password' required/>
+            <input type="password" name='password' value='password' onChange={handleChange} required/>
 
             <label>Confirm Password</label>
-            <input type="password" name='password' required/>
+            <input type="password" name='password' value='' required/>
 
             <div className='form-buttons'>
                 <button type='submit' className="btn-submit">Create</button>
