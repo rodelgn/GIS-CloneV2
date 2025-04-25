351f91b40d54434f6e6d65928ef0e30071cafbbb
Rodel <rodelg.nercuit@gmail.com>
2025-04-25 10:35:15 +0800

    Account creation update with api post method

--- src/components/AccountCreation.jsx
+++ src/components/AccountCreation.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './styles/createUser.css';
+import Swal from 'sweetalert2';
+import Axios from '../api/Axios';
 
 const AccountCreation = ({ onClose }) => {
     const [formData, setFormData] = useState({
@@ -14,25 +16,51 @@ const AccountCreation = ({ onClose }) => {
             ...formData,
             [name]: value
         })
-    }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('Submitting:', formData);
+
+        try {
+            const response = await Axios.post('/userDetail', {
+                name: formData.name,
+                email: formData.email,
+                password: formData.password
+            });
+            console.log(response.data);
+            if (response.data.status === 'ok') {
+                Swal.fire({
+                    title: "Account successfuly created!",
+                    icon: "success",
+                    draggable: false
+                });
+                onClose();
+            }
+        } catch (error) {
+            console.error('Account creation failed:', error);
+            alert('Error creating account. Please try again.');
+        }
+    };
 
 
   return (
     <div className='modal-overlay' onClick={onClose}>
         <div className='modal-content' onClick={(e) => e.stopPropagation()}>
         <h1>Create Account</h1>
-        <form action="submit" className='account-form'>
+        
+        <form action="submit" className='account-form' onSubmit={handleSubmit}>
             <label>Name</label>
-            <input type="text" name='name' onChange={handleChange} required/>
+            <input type="text" name='name' value={formData.name} onChange={handleChange} required/>
 
             <label>Email</label>
-            <input type="text" name='email' onChange={handleChange} required/>
+            <input type="text" name='email' value={formData.email} onChange={handleChange} required/>
 
             <label>Password</label>
-            <input type="password" name='password' onChange={handleChange} required/>
+            <input type="password" name='password' value={formData.password} onChange={handleChange} required/>
 
-            <label>Confirm Password</label>
-            <input type="password" name='password' required/>
+            {/* <label>Confirm Password</label>
+            <input type="password" name='password' required/> */}
 
             <div className='form-buttons'>
                 <button type='submit' className="btn-submit">Create</button>
