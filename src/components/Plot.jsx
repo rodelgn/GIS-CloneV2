51c209478dd5011554df10047845e84738b8584c
Rodel <rodelg.nercuit@gmail.com>
2025-05-04 12:23:08 +0800

    Plotting form styling update

--- src/components/Plot.jsx
+++ src/components/Plot.jsx
@@ -8,74 +8,104 @@ const Plot = ( props ) => {
   return (
     <div className='plot-container'>
       <h1>Plot Parcel</h1>
-      <form action="plot-form" >
-          <div className='uploadWrapper'>
-            <label htmlFor="">Upload CSV File for Auto Fill</label>
-            <input type="file" accept=".csv" required/>
-          </div>
-
-        <div className='plot-data'>
-          <label>Title No.</label>
-          <input type="text" required/>
-          <label>Title Name</label>
-          <input type="text" required/>
-          <label>Date</label>
-          <input type="date" required/>
-          <label>Survey No.</label>
-          <input type="text" required/>
-          <label htmlFor="">Lot No.</label>
-          <input type="text" required/>
-          <label htmlFor="">Blk No.</label>
-          <input type="text" required/>
-          <label>Area (sq.m.)</label>
-          <input type="text" required/>
+      <form action="plot-form">
+  
+        <div className='form-group upload-group'>
+          <label>Upload CSV File for Auto Fill</label>
+          <input type="file" accept=".csv" required />
         </div>
-
-        <div className='boundaries-cntnr'>
-        <h4>Boundaries</h4>
-        <br/>
-
-          <div className='plot-monument'>
-            <label>Monument</label>
-            <input type="text" />
-
-            <label>Easting</label>
-            <input type="text" />
-
-            <label>Northing</label>
-            <input type="text" />
-          </div>
-
-          <div className=''>
-            <div style={{ display: 'flex', gap: '10px' }}>
-            <p>Number of Points</p>
-            <input style={{ width: '55px', borderRadius: '8px' }} type="text" name="" id="" />
+  
+        <div className="section">
+          <h3 style={{ marginBottom: '1rem' }}>Plot Details</h3>
+          <div className='two-column'>
+            <div className="form-group">
+              <label>Title No.</label>
+              <input type="text" required />
             </div>
-
-            <div className='plot-monument'>
-            <label>Tie Line - 1</label>
-            <select name="" id=""></select>
-            <input type="text" />
-            <input type="text" />
-            <select name="" id=""></select>
-            <input type="text" placeholder='Distance' />
+            <div className="form-group">
+              <label>Title Name</label>
+              <input type="text" required />
             </div>
-
-            <div className='btnDraw-ctn'>
-            <button type='submit' className='btn-draw'>Draw</button>
+            <div className="form-group">
+              <label>Date</label>
+              <input type="date" required />
+            </div>
+            <div className="form-group">
+              <label>Survey No.</label>
+              <input type="text" required />
+            </div>
+            <div className="form-group">
+              <label>Lot No.</label>
+              <input type="text" required />
+            </div>
+            <div className="form-group">
+              <label>Blk No.</label>
+              <input type="text" required />
+            </div>
+            <div className="form-group">
+              <label>Area (sq.m.)</label>
+              <input type="text" style={{ width: '130px' }} required />
             </div>
           </div>
         </div>
+  
+        <div className='section'>
+          <h3 style={{ marginBottom: '1rem' }}>Boundaries</h3>
+  
+          <div className='monument-row'>
+            <div className='form-group'>
+              <label>Monument</label>
+              <input type="text" style={{ width: '150px' }} required/>
+            </div>
+            <div className='form-group'>
+              <label>Easting</label>
+              <input type="text" style={{ width: '150px' }} required/>
+            </div>
+            <div className='form-group'>
+              <label>Northing</label>
+              <input type="text" style={{ width: '150px' }} required/>
+            </div>
+          </div>
 
-        <label htmlFor="">Plus Code</label>
-        <input type="text" />
+          <div className='form-group' style={{ marginBottom: '1rem', marginTop: '1rem' }}>
+              <label>Number of Points</label>
+              <input type="text" style={{ width: '80px' }} />
+            </div>
+  
+          <div className='form-group'>
+            <label>Tie Line - 1*</label>
+              <div className='tie-line-row'>
+                <select>
+                  <option value=""></option>
+                  <option value="">N</option>
+                  <option value="">S</option>
+                </select>
+                <input type="text" />
+                <input type="text" />
+                <select>
+                  <option value=""></option>
+                  <option value="">E</option>
+                  <option value="">W</option>
+                </select>
+                <input type="text" placeholder="DISTANCE" />
+                <button className='remove-btn'>x</button>
+              </div>
+              <div className='btnDraw-ctn'>
+                <button className='btn-draw'>Draw</button>
+              </div>
+          </div>
+        </div>
 
+        <div className="form-group">
+          <label>Plus Code</label>
+          <input type="text" />
+        </div>
+  
         <div className='form-buttons'>
           <button type='submit' className="btn-submit">Save</button>
           <button type="button" className="btn-cancel" onClick={props.onClose}>Cancel</button>
         </div>
       </form>
-
     </div>
   )
 }
