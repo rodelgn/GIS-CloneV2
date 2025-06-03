f8141c533688f500f20093cc4d35dbbdd7b44428
Rodel Nercuit <rodelg.nercuit@gmail.com>
2025-06-03 20:51:52 +0800

    Added KML component

---
+++ src/components/Kml.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Kml = () => {
+
+    return (
+        <div>
+            <label htmlFor="">Upload KML File.</label>
+            <input type="file" accept=".csv" />
+        </div>
+    );
+};
+
+export default Kml;
