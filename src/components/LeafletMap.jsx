763695c8d09bc25f61ccad0a0a520798e92e0179
Rodel Nercuit <rodelg.nercuit@gmail.com>
2025-06-10 11:02:17 +0800

    Leaflet Initial update

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { TileLayer } from 'leaflet';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
