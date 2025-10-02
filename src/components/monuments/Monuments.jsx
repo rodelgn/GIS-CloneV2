621d58986f12c549bd56b04a141b56c68e1ea12b
Rodel <rodelg.nercuit@gmail.com>
2025-10-02 15:22:48 +0800

    Monument table initial update

--- src/components/monuments/Monuments.jsx
+++ src/components/monuments/Monuments.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Axios from '../../api/Axios'
 
 const Monuments = () => {
+
+    const fetchMonuments = async () => {
+        try {
+                const response = await Axios.get('/monumentData');
+                console.log('Monuments data:', response.data);
+            } catch (error) {
+                console.error('Error fetching monuments:', error);
+            }
+    }
+
+
+
   return (
-    <div>Monuments</div>
+    <div>
+        <h1>Monuments</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Monument</th>
+                    <th>Easting</th>
+                    <th>Northing</th>
+                </tr>
+            </thead>
+        </table>
+
+        
+        
+    </div>
   )
 }
 
