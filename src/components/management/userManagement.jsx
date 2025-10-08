c715442386860b6ac4ce5adff61ac50a9a251b28
Rodel <rodelg.nercuit@gmail.com>
2025-10-09 00:13:18 +0800

    User management initial update

--- src/components/management/userManagement.jsx
+++ src/components/management/userManagement.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Axios from '../../api/Axios';
 
 const userManagement = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        Axios.get('/userDetail').then((response) => {
+            setUsers(response.data);
+            console.log("User Data: ", response.data)
+        }).catch((error) => {
+            console.error("Error fetching user data: ", error);
+        });
+    }, []);
+
+
   return (
     <div>userManagement</div>
   );
